8a8cbb7fd97fa061b62eb6de81e0214395aae1c7
Tóth Máté <tothmate911@gmail.com>
2020-06-15 12:05:05 +0200

    create pathMethodPairs Map

--- src/main/java/Routes.java
+++ src/main/java/Routes.java
@@ -3,12 +3,12 @@ import com.sun.net.httpserver.HttpExchange;
 public class Routes {
 
     @WebRoute("/test1")
-    public static String test1(HttpExchange exchange) {
+    public static String test1() {
         return "test1";
     }
 
     @WebRoute("/test2")
-    public static String test2(HttpExchange exchange) {
+    public static String test2() {
         return "test2";
     }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,38 +3,42 @@ import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Test {
+    private static Map<String, Method> pathMethodPairs = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/home", new MyHandler());
-        server.createContext("/test1", new MyHandler());
-        server.createContext("/test2", new MyHandler());
-        server.setExecutor(null); // creates a default executor
+
+        for (Method method : Routes.class.getMethods()) {
+            if (method.isAnnotationPresent(WebRoute.class)) {
+                WebRoute webRoute = method.getAnnotation(WebRoute.class);
+                String path = webRoute.value();
+                pathMethodPairs.put(path, method);
+                server.createContext(path, new MyHandler());
+            }
+        }
+        server.setExecutor(null);
         server.start();
     }
 
     static class MyHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "default";
+            String response = "default response";
 
-            for (Method method : Routes.class.getMethods()) {
-                if (method.isAnnotationPresent(WebRoute.class)) {
-                    WebRoute webRoute = method.getAnnotation(WebRoute.class);
-                    if (webRoute.value().equals(exchange.getRequestURI().toString())) {
-                        try {
-                            response = (String) method.invoke(null, exchange);
-                        } catch (IllegalAccessException | InvocationTargetException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+            Method method = pathMethodPairs.get(exchange.getRequestURI().toString());
+            try {
+                response = (String) method.invoke(null);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
             }
+
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
